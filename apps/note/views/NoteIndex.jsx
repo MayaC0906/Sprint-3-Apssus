041190261e55f2bf27634e8c9f12ed1b26cd07b1
Maya <mayac8771108@gmail.com>
2023-09-06 14:32:06 +0300

    Added main layout

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,4 +1,16 @@
+import { Notes } from "../cmps/Notes.jsx"
+import { SideNav } from "../cmps/SideNav.jsx"
+
 
 export function NoteIndex() {
-    return <div>note app</div>
+    return (
+        <section className="main-layout">
+            <section className="main-screen">
+                <Notes />
+            </section>
+            <section className="side-nav">
+                <SideNav />
+            </section>
+        </section>
+    )
 }
