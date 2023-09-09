ccc6b935b7f0f509bffd0a6f3863eb3bd67bf3da
Maya <mayac8771108@gmail.com>
2023-09-09 21:22:53 +0300

    homePage

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,12 @@
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+        <h1>Welcome to our home page!</h1>
+        <h2>This Apps were created with love ❤️</h2>
+        <section className="home-images">
+            <img src="./assets/img/gmailLogo.png" />
+            <img src="./assets/img/googleKeep.png" />
+        </section>
+        <h3>© All rights reserved to Elior Mangistu & Maya Cohen</h3>
     </section>
 }
